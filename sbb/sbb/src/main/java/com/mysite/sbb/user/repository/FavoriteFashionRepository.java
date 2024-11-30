3bb2985948d4c956a5caef6dc82594c6fd82ad9f
jjsik <163238332+jjsik@users.noreply.github.com>
2024-11-30 23:49:26 +0900

    Delete sbb/sbb directory

--- sbb/sbb/src/main/java/com/mysite/sbb/user/repository/FavoriteFashionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mysite.sbb.user.repository;
-
-import com.mysite.sbb.user.DTO.FavoriteFashionResultDTO;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FavoriteFashionRepository extends JpaRepository<FavoriteFashionResultDTO, Long> {
-
-}
